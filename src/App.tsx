1b7c5169afab0213e0c873473bb4b15fc0fa85ec
benak <benakkishanj@gmail.com>
2025-08-15 12:07:17 +0530

    Added firebase SDK and dotenv for config

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-3xl font-extrabold text-blue-300 ">
         Welcome to Nativewind!
       </Text>
     </View>
